c4ab062299b9d63d29fdc0c2812ec7b3483a47d2
code-walker-23 <chhabravinay549@gmail.com>
2024-07-22 17:11:32 +0530

    Live Api Updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,34 @@ import resList from "../utils/mockData";
 import { useState } from "react";
 import { useEffect } from "react";
 
+
+
+
 const Body = () => {
+const arr = useState([]);
+let [resFilter, setListOfRestaurant] = arr;
+/* 
+
+This Callback function in useEffect is called when the component is mounted or rendered for the first time.  
+It will keep the callback function and called when the component is rendered.
+
+*/
+  useEffect(() => {
+    fectchData(); // our browser will block the request because of CORS policy.
+  },[]);
+
+  console.log("Body Component is Rendered!");
+
+  const fectchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurant(json.data.cards[1]?.card?.card?.gridElements
+?.infoWithStyle?.restaurants);
+    
+  };
+
   /* 
   Local state varibale.
   Array Destructuring.
@@ -17,8 +44,8 @@ const Body = () => {
   let [resFilter,setListOfRestaurant] = arr;
 
   */
-  const arr = useState(resList);
-  let [resFilter, setListOfRestaurant] = arr;
+
+ 
   return (
     <div className="body">
       <button
